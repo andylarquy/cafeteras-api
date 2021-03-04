0bc9101e011f3950d254c60b84c746295a852981
Andres Suarez <andres27059934@gmail.com>
2021-03-04 14:19:57 -0300

    Agregadas cafeteras

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 4848
+const cafeteras = require('./cafeteras.json')
+
+const getCafeteraById = cafeteraId => cafeteras.find(cafetera => cafetera.id === cafeteraId)
 
 app.get('/', (req, res) => {
-  res.send('Proyecto super ultra secreto de la NASA')
+  res.send('La Cafetera API esta operativa! :D')
+})
+
+app.get('/cafeteras', (req, res) => {
+  res.json(cafeteras)
+})
+
+app.get('/cafetera/:cafeteraId', (req, res) => {
+  try {
+    const cafetera = getCafeteraById(req.params.cafeteraId)
+    if (cafetera) {
+      res.json(cafetera)
+    } else {
+      res.status(404).json({ message: 'No se pudo encontrar una cafetera con ese ID' })
+    }
+
+  } catch (e) {
+    res.status(400).json({ message: 'Algo salio mal al intentar obtener esa cafetera' })
+  }
 })
 
 app.listen(port, () => {
